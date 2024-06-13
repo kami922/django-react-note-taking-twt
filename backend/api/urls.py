7250cad806237cd99b7ebdb90033654cb1e65a08
kami922 <kamranulhaq2002@gmail.com>
2024-06-13 20:29:16 +0500

    started frontend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path,include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenRefreshView,TokenObtainPairView
+from . import views
 
+
+urlpatterns = [
+    path("notes/",views.NoteListCreate.as_view(),name='note-list'),
+    path("notes/delete/<int:pk>",views.NoteDelete.as_view(),name="delete-note")
+]
 
